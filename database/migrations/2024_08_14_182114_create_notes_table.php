d833ff92840b54a003e412598c830f6d6933228b
fernando.brandan <fernando.brandan1@hotmail.com>
2024-08-14 15:40:35 -0300

    Notes - Create  model db migration seede factory

--- database/migrations/2024_08_14_182114_create_notes_table.php
+++ database/migrations/2024_08_14_182114_create_notes_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->string('content', 255)->nullable();
+            $table->boolean('done')->default(false);
+            $table->enum('priority', ['low', 'medium', 'high'])->default('low');
             $table->timestamps();
         });
     }
